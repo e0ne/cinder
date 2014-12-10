41b1d4fa155c7f5d95efeab2a19c0498474d5d1f
Walter A. Boring IV <walter.boring@hp.com>
2014-12-10 01:03:39 +0000

    Add volume multi attach support
    This patch includes the Cinder changes needed
    to support volume multiple attaches.  Nova and
    python-cinderclient also need patches associated
    to provide support for multiple attachments.
    
    This adds the multiattach flag to volumes.  When a
    volume is created, a multiattach flag can be set,
    which allows a volume to be attached to more than
    one Nova instance or host.  If the multiattach flag is
    not set on a volume, it cannot be attached to more
    than one Nova instance or host
    
    Each volume attachment is tracked in a
    new volume_attachment table.  The attachment id is
    the unique identifier for each attachment to an
    instance or host.
    
    When a volume is to be detached the attachment
    uuid must be passed in to the detach call in
    order to determine which attachment should be
    removed.  Since a volume can be attached to an
    instance and a host, the attachment id is used
    as the attachment identifier.
    
    Nova:
    https://review.openstack.org/#/c/153033/
    https://review.openstack.org/#/c/153038/
    
    python-cinderclient:
    https://review.openstack.org/#/c/85856/
    
    Change-Id: I950fa00ed5a30e7758245d5b0557f6df42dc58a3
    Implements: blueprint multi-attach-volume
    APIImpact

--- cinder/tests/test_migrations.py
+++ cinder/tests/test_migrations.py
@@ -791,6 +791,41 @@ class MigrationsMixin(test_migrations.WalkVersionsMixin):
                 shadow_name = "shadow_" + table_name
                 self.assertIn(shadow_name, table_names)
 
+    def _check_41(self, engine, data):
+        volumes = db_utils.get_table(engine, 'volumes')
+        self.assertNotIn('instance_uuid', volumes.c)
+        self.assertNotIn('attached_host', volumes.c)
+        self.assertNotIn('attach_time', volumes.c)
+        self.assertNotIn('mountpoint', volumes.c)
+        self.assertIsInstance(volumes.c.multiattach.type,
+                              self.BOOL_TYPE)
+
+        attachments = db_utils.get_table(engine, 'volume_attachment')
+        self.assertIsInstance(attachments.c.attach_mode.type,
+                              sqlalchemy.types.VARCHAR)
+        self.assertIsInstance(attachments.c.instance_uuid.type,
+                              sqlalchemy.types.VARCHAR)
+        self.assertIsInstance(attachments.c.attached_host.type,
+                              sqlalchemy.types.VARCHAR)
+        self.assertIsInstance(attachments.c.mountpoint.type,
+                              sqlalchemy.types.VARCHAR)
+        self.assertIsInstance(attachments.c.attach_status.type,
+                              sqlalchemy.types.VARCHAR)
+
+    def _post_downgrade_041(self, engine):
+        self.assertFalse(engine.dialect.has_table(engine.connect(),
+                                                  "volume_attachment"))
+        volumes = db_utils.get_table(engine, 'volumes')
+        self.assertNotIn('multiattach', volumes.c)
+        self.assertIsInstance(volumes.c.instance_uuid.type,
+                              sqlalchemy.types.VARCHAR)
+        self.assertIsInstance(volumes.c.attached_host.type,
+                              sqlalchemy.types.VARCHAR)
+        self.assertIsInstance(volumes.c.attach_time.type,
+                              sqlalchemy.types.VARCHAR)
+        self.assertIsInstance(volumes.c.mountpoint.type,
+                              sqlalchemy.types.VARCHAR)
+
     def test_walk_versions(self):
         self.walk_versions(True, False)
 
