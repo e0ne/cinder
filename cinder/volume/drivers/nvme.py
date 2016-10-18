0bde8c496a81cb14635491b48ab5eef474de68be
Ivan Kolodyazhny <e0ne@e0ne.info>
2016-10-18 19:04:36 +0000

    Make NVMe Driver (PoC) working

--- cinder/volume/drivers/nvme.py
+++ cinder/volume/drivers/nvme.py
@@ -19,6 +19,8 @@ from os_brick.target.nvme.rpc import resources
 from oslo_config import cfg
 from oslo_log import log as logging
 
+from cinder import context
+from cinder.db.sqlalchemy import api
 from cinder import interface
 from cinder.volume import driver
 
@@ -29,7 +31,7 @@ volume_opts = [
                default='127.0.0.1',
                help='NVMe Target node IP'),
     cfg.IntOpt('target_port',
-               default=5260,
+               default=4430,
                help='NVMe Target node port')
 ]
 
@@ -47,15 +49,31 @@ class NVMeDriver(driver.VolumeDriver):
 
     def __init__(self, *args, **kwargs):
         super(NVMeDriver, self).__init__(*args, **kwargs)
-        self.target = resources.NVMeTargetObject()
+#        self.target = resources.NVMeTargetObject(self.configuration.target_ip, self.configuration.target_port)
+        self.target = resources.NVMeTargetObject('10.100.1.95', 5260)
+        self.backend_name = 'NVMe'
+
+    def check_for_setup_error(self):
+        nvmfs = self.target.get_nvmf_subsystems(None)
 
     def create_volume(self, volume):
+        all_subsystems = self.target.get_nvmf_subsystems(None)
+        nvme_subsystems = filter(lambda x: x['subtype'] == 'NVMe', all_subsystems)
+        lst = api.volume_get_all_by_host(context.get_admin_context(),
+                                         self.host)
+        nqns = [subsystem['nqn'] for subsystem in nvme_subsystems]
         name = volume['name']
-        try:
-            self.target.construct_aio_lun(name)
-        except Exception as e:
-            LOG.error(
-                'Problem with volume %s creation: %s', name, e['message'])
+        for vol in lst:
+            if volume['provider_location']:
+                if volume['provider_location'] in nqns:
+                    nqns.remove(volume['provider_location'])
+
+        if not len(nqns):
+            raise exception.CinderException(_("No free subsystem"))
+
+        volume['provider_location'] = nqns[0]
+        volume.save()
+        return {'provider_location': nqns[0]}
 
     def delete_volume(self, volume):
         name = volume['name']
@@ -69,17 +87,40 @@ class NVMeDriver(driver.VolumeDriver):
         return {
             'driver_volume_type': self.PROTOCOL,
             'data': {
-                'target_ip': self.conf.target_ip,
-                'target_port': self.conf.target_port
+                'target_portal': '10.100.1.95',
+                'target_port': 4420,
+                'nqn': volume['provider_location']
             }
         }
 
-    def get_volume_stats(self):
-        return {
-            'volume_backend_name': '',
-            'vendor_name': '',
+    def get_volume_stats(self, refresh=False):
+        data = {
+            'volume_backend_name': self.backend_name,
+            'vendor_name': "Intel",
             'driver_version': self.VERSION,
-            'storage_protocol': self.PROTOCOL,
-            'total_capacity_gb': '',
-            'free_capacity_gb': ''
+            'storage_protocol': 'rdma',
+            'pools': []}
+
+        single_pool = {
+            'pool_name': data['volume_backend_name'],
+            'total_capacity_gb': 'infinite',
+            'free_capacity_gb': 'infinite',
+            'QoS_support': False}
+
+        data['pools'].append(single_pool)
+        return data
+
+    def create_export(self, context, volume, connector):
+        return {
+            'target_portal': '10.100.1.95',
+            'nqn':  volume['provider_location'],
+            'target_port': 4420
         }
+
+    def remove_export(self, context, volume):
+        volume['provider_location'] = None
+        # TODO (e0ne): fix save call
+        # volume.save()
+
+    def ensure_export(self, context, volume):
+        pass
